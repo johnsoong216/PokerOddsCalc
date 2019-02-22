dcc36a9268741e2b0e65d63ee9a6e7233860d508
songqien <johnsqe.song@mail.utoronto.ca>
2019-02-22 17:58:00 -0500

    Card Model

--- card.py
+++ card.py
@@ -1,2 +1,22 @@
 class Card:
+    """
+    A standard poker card with value and suit
+    Suit:
+    - Diamond 1
+    - Club 2
+    - Heart 3
+    - Spade 4
+    
+    
+    Value:
+    - 
+    """
+    
+    def __init__(self, suit:int, value:int) -> None:
+        self.suit = suit
+        self.value = value
+    
+    
+    def __equals__(self, other:Card) -> bool:
+        return self.value == other.value
     
