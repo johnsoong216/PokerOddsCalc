6190efd92b83b1e3c30448243452235a6b3a7e41
songqien <johnsqe.song@mail.utoronto.ca>
2019-02-22 19:15:04 -0500

    Finished Deck and Card Models

--- card.py
+++ card.py
@@ -7,16 +7,32 @@ class Card:
     - Heart 3
     - Spade 4
     
-    
     Value:
-    - 
+    - Two 2
+    ....
+    - King 13
+    - Ace 14
     """
     
-    def __init__(self, suit:int, value:int) -> None:
+    def __init__(self, value:int, suit:int) -> None:
         self.suit = suit
         self.value = value
     
+    def __equals__(self, other) -> bool:
+        if isinstance(other, Card):
+            return self.value == other.value
+        return false
+        
     
-    def __equals__(self, other:Card) -> bool:
-        return self.value == other.value
+    
+    def __str__(self) -> str:
+        suitStrings = {1: 'd', 2: 'c', 3: 'h', 4: 's'}
+        numStrings = {2: '2', 3: '3', 4: '4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9',
+                      10: '10', 11: 'J', 12: 'Q', 13:'K', 14:'A'}
+        return numStrings.get(self.value) + suitStrings.get(self.suit)
+
+
+if __name__ == '__main__':
+    a = Card(7, 3)
+    print(a)
     
